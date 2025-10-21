1069397c416a16866259ce2c67889698c89095f5
GreedyGobling <greedygobling@gmail.com>
2025-10-21 20:42:59 +0200

    char[][]

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,10 @@
 public class Cell {
-}
+    public char[][] grid = new char[6][6];
 
+    // Constructor to initialize the grid with cells
+    // do place bombs aldredy or is it done later?
+    // like then guessing a cell
+
+
+}
 //Cell logic
