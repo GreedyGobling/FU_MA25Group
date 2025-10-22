ba11b319acaa88f3b5e9a6bb604fef32410d483d
GreedyGobling <greedygobling@gmail.com>
2025-10-22 20:52:30 +0200

    place mines at start

--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,39 @@
+import java.util.Random;
+
 public class Cell {
     public int rows;
     public int cols;
+    public int mines;
+    public boolean[][] isMine;
+    public int[][] adjacentMines;
+    public int revealedCells;
+    public int minesLeft;
 
-    public void takeInput(int rowNumber, int colNumber){
-        System.out.println("Input taken: " + rowNumber + ", " + colNumber);
+    public void takeInput(int rowInput, int colInput){
+        System.out.println("Input taken: " + rowInput + ", " + colInput);
+    }
 
+    public void MineSweeperGame(int rowNumber, int colNumber, int mines){
+        this.rows = rowNumber;
+        this.cols = colNumber;
+        this.mines = mines;
+        this.isMine = new boolean[rows][cols];
+        this.adjacentMines = new int[rows][cols];
+        System.out.println("MineSweeper Game initialized with rows: " + rows + " and cols: " + cols);
+
+        placeMines();
+    }
+
+    public void placeMines(){
+        Random random = new Random();
+        int minesPlaced = 0;
+        while (minesPlaced < mines) {
+            int r = random.nextInt(rows);
+            int c = random.nextInt(cols);
+            if (!isMine[r][c]) {
+                isMine[r][c] = true;
+                minesPlaced++;
+            }
+        }
     }
 }
