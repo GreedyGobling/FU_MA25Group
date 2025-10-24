deb6f8a9c0d229fd7e2d47db0f7115d207aaad46
GreedyGobling <greedygobling@gmail.com>
2025-10-24 09:00:23 +0200

    use enum states

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,12 @@
 public class Cell {
+    public static CellState Unreavealed = CellState.Unrevealed;
+    public static CellState Revealed = CellState.Revealed;
+    public static CellState Flagged = CellState.Flagged;
+
+    private boolean isMine;
+    private CellState state = Unreavealed;
+    private int count = 0; // Only set on reveal
+
 }
 
 //Cell logic
