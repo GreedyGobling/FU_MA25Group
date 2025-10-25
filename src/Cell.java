492b431038bbe4b010199dae7dc23f752146e313
GreedyGobling <greedygobling@gmail.com>
2025-10-25 15:25:21 +0200

    combo of v1 and v2

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,12 @@
+import java.util.Random;
+
 public class Cell {
+    public int rows;
+    public int cols;
+    public int mines;
+    public boolean[][] isMine;
+    public int[][] adjacentMines;
+
     // COMBO OF Version 1 and 2
     public static CellState Unrevealed = CellState.Unrevealed;
     public static CellState Revealed = CellState.Revealed;
@@ -6,11 +14,17 @@ public class Cell {
 
     // function to spawn and sett // used at start
     // enum states easy to hide and show cell
+    public void starting (int rowNumber, int colNumber){
+        this.rows = rowNumber;
+        this.cols = colNumber;
+        this.mines = mines;
 
+    }
     // spawn mines
 
     // number calculator
 
+    //
 }
 
 //Cell logic
