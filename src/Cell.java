36ab6e9a421fb8484fefb8f506e7ae9eb4c8db29
GreedyGobling <greedygobling@gmail.com>
2025-10-25 14:47:14 +0200

    combo of v1 and v2

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,16 @@
 public class Cell {
+    // COMBO OF Version 1 and 2
+    public static CellState Unrevealed = CellState.Unrevealed;
+    public static CellState Revealed = CellState.Revealed;
+    public static CellState Flagged = CellState.Flagged;
+
+    // function to spawn and sett // used at start
+    // enum states easy to hide and show cell
+
+    // spawn mines
+
+    // number calculator
+
 }
 
 //Cell logic
