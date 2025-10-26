0355a89a7e2ba4f15466328491676eb2b18337c5
GreedyGobling <greedygobling@gmail.com>
2025-10-26 02:40:57 +0100

    set all cell to unrevealed state

--- src/Cell.java
+++ src/Cell.java
@@ -6,57 +6,64 @@ public class Cell {
     private int mines;
     private boolean[][] isMine;
     private int[][] adjacentMines;
-    private double mineDensity = 0.12;
+    private double mineDensity = 0.12; // default 0.12
     private CellState[][] states;
 
     // function to spawn and sett // used at start
     // enum states easy to hide and show cell
-    public void initilize(int rowNumber, int colNumber){
+    public void initilize(int rowNumber, int colNumber) {
         this.rows = rowNumber;
         this.cols = colNumber;
         mines = amountMines(rowNumber, colNumber);
         isMine = new boolean[rows][cols];
         adjacentMines = new int[rows][cols];
 
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                states = new CellState[rows][cols];
+                states[r][c] = CellState.Unrevealed;
+            }
+        }
+
         placeMines();
         calculateAdjacent();
     }
 
-    private int amountMines(int row, int col){
-    int totalCells = row * col;
-    int mines = (int) Math.round(totalCells * mineDensity);
-    return mines;
 
+
+    private int amountMines(int row, int col) {
+        int totalCells = row * col;
+        int mines = (int) Math.round(totalCells * mineDensity);
+        return mines;
     }
     // number calculator
 
-    private void placeMines(){
+    private void placeMines() {
         Random random = new Random();
         int minesPlaced = 0;
-        while (minesPlaced < mines){
+        while (minesPlaced < mines) {
             int r = random.nextInt(rows);
             int c = random.nextInt(cols);
-            if (!isMine[r][c]){
+            if (!isMine[r][c]) {
                 isMine[r][c] = true;
                 minesPlaced++;
             }
         }
     }
 
-    private void calculateAdjacent(){
-        for (int r = 0; r < rows; r++){
-            for (int c = 0; c < cols; c++){
-                if (isMine[r][c]){
+    private void calculateAdjacent() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (isMine[r][c]) {
                     adjacentMines[r][c] = -1; // it's a trap
-                }
-                else {
+                } else {
                     int count = 0;
-                    for (int dr = -1; dr <= 1; dr++){
-                        for (int dc = -1; dc <= 1; dc++){
+                    for (int dr = -1; dr <= 1; dr++) {
+                        for (int dc = -1; dc <= 1; dc++) {
                             int nr = r + dr;
                             int nc = c + dc;
-                            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols){
-                                if (isMine[nr][nc]){
+                            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+                                if (isMine[nr][nc]) {
                                     count++;
                                 }
                             }
